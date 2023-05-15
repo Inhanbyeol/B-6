d392272bb1b22cc69d12d5f19a6b01e7cff8cf65
inhanbyeol <inhanbyeol94@gmail.com>
2023-05-15 17:25:49 +0900

    app.py flask, mongodb 연동

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print('get_test_jw')
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://b6:TEJ6JD6Xn9IxLfLg@cluster0.3ptd6x1.mongodb.net/?retryWrites=true&w=majority')
+db = client.data
+
+@app.route('/')
+def home():
+  return render_template('index.html')
+
+if __name__ == '__main__':
+  app.run('0.0.0.0', port=3000, debug=True)
