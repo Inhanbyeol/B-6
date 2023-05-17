b9a860e79cd72d3a8dd99257369e7d3dfd083345
choisumin <tnals634@naver.com>
2023-05-17 15:34:48 +0900

    수정

--- app.py
+++ app.py
@@ -23,5 +23,36 @@ def createUsers():
     else :
        return jsonify(0)
 
+#정보 출력
+@app.route("/user", methods=["GET"])
+def create_get():
+    all_comments = list(db.users.find({},{'_id':False}))
+
+    return jsonify({'result':all_comments,'msg':'get!'})
+
+#데이터 저장
+@app.route("/testc", methods=["POST"])
+def testc_post():
+    name_info = request.form['name']
+    id_receive = request.form['id_give']
+    comment_receive = request.form['comment_give']
+
+    doc = {
+        'name':name_info,
+        'id':id_receive,
+        'comment':comment_receive
+    }
+    db.testc.insert_one(doc)
+
+    return jsonify({'msg': '1'})
+
+#데이터 호출
+@app.route("/testc", methods=["GET"])
+def testc_get():
+    all_comments = list(db.testc.find({},{'_id':False}))
+
+    return jsonify({'result':all_comments,'msg':'get!'})
+
+
 if __name__ == '__main__':
   app.run('0.0.0.0', port=3000, debug=True)
